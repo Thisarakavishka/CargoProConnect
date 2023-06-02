4ac3f7956d4c293b9c042990c0fb98d866ca924b
thisara <thisarakavishka0@gmail.com>
2023-06-02 18:26:52 +0530

    Add CrudDAO and create dao package

---
+++ src/main/java/lk/ijse/cargoproconnect/dao/CrudDAO.java
@@ -0,0 +1,21 @@
+package lk.ijse.cargoproconnect.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO<T> {
+
+    public ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+
+    public boolean add(T dto) throws SQLException, ClassNotFoundException;
+
+    public boolean update(T dto) throws SQLException, ClassNotFoundException;
+
+    public boolean exist(String id) throws SQLException, ClassNotFoundException;
+
+    public boolean delete(String id) throws SQLException, ClassNotFoundException;
+
+    public T search(String id) throws SQLException, ClassNotFoundException;
+
+    public String generateNewId() throws SQLException, ClassNotFoundException;
+}
