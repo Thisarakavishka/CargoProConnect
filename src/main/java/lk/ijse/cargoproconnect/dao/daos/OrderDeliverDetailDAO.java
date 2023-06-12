60118aad06ccd6d8291538bf1abf39a16757c4bf
thisara <thisarakavishka0@gmail.com>
2023-06-12 10:14:13 +0530

    Implement OrderDeliverDetailDAOImpl and refactor OrderDeliverDetailDAO

--- src/main/java/lk/ijse/cargoproconnect/dao/daos/OrderDeliverDetailDAO.java
+++ src/main/java/lk/ijse/cargoproconnect/dao/daos/OrderDeliverDetailDAO.java
@@ -3,5 +3,10 @@ package lk.ijse.cargoproconnect.dao.daos;
 import lk.ijse.cargoproconnect.dao.CrudDAO;
 import lk.ijse.cargoproconnect.entity.OrderDeliverDetails;
 
+import java.sql.SQLException;
+
 public interface OrderDeliverDetailDAO extends CrudDAO<OrderDeliverDetails> {
+
+    boolean addOrderDeliveryDetails(String orderId, String deliverId) throws SQLException;
+
 }
---
+++ src/main/java/lk/ijse/cargoproconnect/dao/daos/impl/OrderDeliverDetailDAOImpl.java
@@ -0,0 +1,52 @@
+package lk.ijse.cargoproconnect.dao.daos.impl;
+
+import lk.ijse.cargoproconnect.dao.daos.OrderDeliverDetailDAO;
+import lk.ijse.cargoproconnect.entity.OrderDeliverDetails;
+import lk.ijse.cargoproconnect.util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class OrderDeliverDetailDAOImpl implements OrderDeliverDetailDAO {
+
+    @Override
+    public ArrayList<OrderDeliverDetails> getAll() throws SQLException, ClassNotFoundException {
+        throw new UnsupportedOperationException("This feature is not implemented yet ");
+    }
+
+    @Override
+    public boolean add(OrderDeliverDetails dto) throws SQLException, ClassNotFoundException {
+        throw new UnsupportedOperationException("This feature is not implemented yet ");
+    }
+
+    @Override
+    public OrderDeliverDetails search(String id) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.execute("SELECT * FROM order_deliver_details WHERE deliver_id = ?", id);
+        if (resultSet.next()) {
+            return new OrderDeliverDetails(resultSet.getString(1),resultSet.getString(2));
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(OrderDeliverDetails dto) throws SQLException, ClassNotFoundException {
+        throw new UnsupportedOperationException("This feature is not implemented yet ");
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        throw new UnsupportedOperationException("This feature is not implemented yet ");
+    }
+
+    @Override
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+        throw new UnsupportedOperationException("This feature is not implemented yet ");
+    }
+
+    @Override
+    public boolean addOrderDeliveryDetails(String orderId, String deliverId) throws SQLException {
+        return CrudUtil.execute("INSERT INTO order_deliver_details(order_id, deliver_id) VALUES (?, ?)", orderId, deliverId);
+    }
+    //use search() method for getOrderId(id) and getDeliverId(id)
+}
