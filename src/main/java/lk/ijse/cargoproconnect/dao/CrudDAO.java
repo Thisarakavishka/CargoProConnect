5150bbc51e5b1ddc836008123bbd16e263f60d72
thisara <thisarakavishka0@gmail.com>
2023-06-12 15:09:57 +0530

    Implement SuperDAO

--- src/main/java/lk/ijse/cargoproconnect/dao/CrudDAO.java
+++ src/main/java/lk/ijse/cargoproconnect/dao/CrudDAO.java
@@ -3,7 +3,7 @@ package lk.ijse.cargoproconnect.dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface CrudDAO<T> {
+public interface CrudDAO<T> extends SuperDAO{
 
     ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
 
